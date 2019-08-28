bb96c6d61d2cc94fe742b2dae8bd952ed9604858
roman <romanbalitskij9@gmail.com>
2019-08-28 19:41:20 +0300

    fix bag

--- app/Console/Commands/ParserCraigslist.php
+++ app/Console/Commands/ParserCraigslist.php
@@ -2,15 +2,19 @@
 
 namespace App\Console\Commands;
 
-use App\Models\City;
+use App\Models\Category;
 use App\Models\Country;
-use App\Models\State;
+use App\Traits\Craigslist;
+use App\Traits\TraitCraigslist;
 use GuzzleHttp\Client;
 use Illuminate\Console\Command;
-use KubAT\PhpSimple\HtmlDomParser;
+use Sunra\PhpSimple\HtmlDomParser;
+
 
 class ParserCraigslist extends Command
 {
+
+    use TraitCraigslist ;
     /**
      * The name and signature of the console command.
      *
@@ -42,101 +46,17 @@ class ParserCraigslist extends Command
      */
     public function handle()
     {
+        $url  = 'https://www.craigslist.org/about/sites';
+        $dom  = $this->getContent($url);
+        $urls = $this->getCityList($dom);
+        $this->storeStatistics($urls);
 
-        $url = 'https://www.craigslist.org/about/sites';
-        $client = new Client();
-        $response = $client->request('GET', $url);
-        $response_status_code = $response->getStatusCode();
-        $html = $response->getBody()->getContents();
-        $dom = HtmlDomParser::str_get_html($html);
-
-        $mainPage = $dom->find('section[class="body"]', 0);
-
-        $urls = [];
-        foreach ($mainPage->find('h1 ') as $searchCountry) {
-
-
-            $countryId = Country::createCountry($searchCountry->text());
-
-
-
-            foreach ($mainPage->find('div[class="colmask"]') as $findCategory) {
-
-                foreach ($findCategory->children() as $category) {
-
-                    foreach ($category->find('h4') as $title) {
-
-
-
-                      $stateId = State::createState($category = $title->text(), $countryId);
-
-                        foreach ($title->next_sibling()->find('li a ') as $subcategory) {
-
-
-                            $parseUrl = str_replace(' ', '', $subcategory->href);
-                            $urls[] = $subcategory->href;
-
-                               City::createCity($subcategory->text(),$stateId);
-
-                        }
-                    }
-                }
-            }
-        }
-
-    }
-
-
-    /* парсинг второго потока  housing  service  **/
-    public function parserPages( $url) {
-
-        foreach ($url as $link) {
-
-            $context = stream_context_create(
-                array(
-                    'http' => array(
-                        'max_redirects' => 101
-                    )
-                )
-            );
-            $html = file_get_contents($link, false, $context);
-            $dom = HtmlDomParser::str_get_html($html);
-
-            $cities = explode(',', $dom->find('div[class="regular-area"] h2', 0)->text()); // cities[0]
-            $housings = $dom->find('div[class="housing"] ul[id="hhh0"] li a');
-
-            $data = [];
-            foreach ($housings as $housing) {
-                $data[$housing->href] = $housing->text();
-            }
-
-
-            $servicesLeft = $dom->find('div[class="community"] ul[id="bbb0"] li a');
-            foreach ($servicesLeft as $service) {
-                $data[$service->href] = $service->text();
-            }
-
-            $servicesRight = $dom->find('div[class="community"] ul[id="bbb1"] li a');
-            foreach ($servicesRight as $serviceRight) {
-
-                ///*цю дату можна передавати на інший етап а там вже проходитись по ній** ///
-                $data[$serviceRight->href] = $serviceRight->text();
-            }
-
-        }
-        dump($data);
+        //  dump($createStatistics);
 
 
     }
 
 
-    /***
-     *
-     * нужно будет создать ищо одну базу допустим post
-     *    поле  где будет вставлятся  назва housing services
-     *  поле  количество постов  в даних силках
-     * поле city_id   к которому принадлежит
-     *
-     *
-    ***/
 }
+
+
---
+++ app/Traits/TraitCraigslist.php
@@ -0,0 +1,112 @@
+<?php
+
+
+namespace App\Traits;
+
+
+use App\Models\Advertisement;
+use App\Models\City;
+use App\Models\Country;
+use App\Models\State;
+use KubAT\PhpSimple\HtmlDomParser;
+
+
+trait TraitCraigslist
+{
+
+    public function  getContent($url) {
+        $agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.96 Safari/537.36';
+        $config = '/tmp/cookies.txt';
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_USERAGENT, $agent);
+        curl_setopt($ch, CURLOPT_COOKIEJAR, $config);
+        curl_setopt($ch, CURLOPT_COOKIEFILE, $config);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        $mas_html = curl_exec($ch);
+        curl_close($ch);
+        $dom = HtmlDomParser::str_get_html($mas_html);
+        return $dom;
+    }
+
+    public function getCityList($html){
+
+        $start = $html->find('section[class="body"]', 0);
+        $urls = [];
+
+        foreach ($start->find('h1') as $country) {
+
+            $countryId = Country::createCountry($country->text());
+
+            foreach ($country->next_sibling()->find('h4') as $state) {
+
+                $stateId = State::createState($state->text(), $countryId);
+
+                foreach ($state->next_sibling()->find('li a') as $cities) {
+
+                    $cityId = City::createCity($cities->text(), $stateId);
+                    $urls[$cityId] = $cities->href;
+                }
+            }
+        }
+        return $urls;
+    }
+
+
+    public function storeStatistics($urls){
+
+        foreach ($urls as $id => $linkPagesHousing) {
+
+            $englishLink = $linkPagesHousing.'?lang=en&cc=us';
+            $dom = $this->getContent($englishLink);
+
+
+            /**getHousing**/
+            $housings = $dom->find('div[class="housing"] ul[id="hhh0"] li a');
+            $data = [];
+            foreach ($housings as $housing) {
+                $data[$housing->href] = $housing->text();
+            }
+            /**getServicesLeft*/
+            $servicesLeft = $dom->find('div[class="community"] ul[id="bbb0"] li a');
+            foreach ($servicesLeft as $service) {
+                $data[$service->href] = $service->text();
+            }
+            /**getServicesRight*/
+            $servicesRight = $dom->find('div[class="community"] ul[id="bbb1"] li a');
+            foreach ($servicesRight as $serviceRight) {
+                $data[$serviceRight->href] = $serviceRight->text();
+            }
+
+            /**count the number of ads */
+            foreach ($data as $key => $value) {
+                $prepareLink = ltrim($key,'/');
+                $url =  $linkPagesHousing.$prepareLink;
+                dump($url);
+
+//                $dom = $this->getContent($url);
+                $context = stream_context_create(
+                    array(
+                        'http' => array(
+                            'max_redirects' => 101,
+                            'ignore_errors' => true
+                        )
+                    )
+                );
+                $html = file_get_contents($url, false, $context);
+                $doms = HtmlDomParser::str_get_html($html);
+                $count = $doms->find('span[class="totalcount"]', 0) == null ? '0' : $doms->find('span[class="totalcount"]', 0)->text();
+
+
+                $data = [
+                    'title'    => trim($value,' '),
+                    'quantity' => $count,
+                    'city_id'  => $id
+                ];
+
+                Advertisement::updateOrCreate($data);
+                dump('Advertisement save ');
+            }
+        }
+    }
+}
